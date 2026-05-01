1917a4691e630e0092bffa7136eacf1c249c1976
RocketToast <22530262+RocketToast@users.noreply.github.com>
2026-05-01 11:56:03 -0400

    Set default IconIndex added help
    Updated IconIndex parameter to have a default value of 0.
    
    Added help to the beginning with examples.

--- New-Shortcut.ps1
+++ New-Shortcut.ps1
@@ -1,4 +1,66 @@
+
 function New-ShortCut {
+<#
+.SYNOPSIS
+
+Creates a shortcut on your computer.
+  - .lnk for any shortcut with a driveletter
+  - .url for shortcuts to a website / fileshare
+
+.DESCRIPTION
+
+This function will make a shortcut on your system. If the TargetPath begins with any driveletter
+(ex: C:\, Z:\, A:\) then a `.lnk` shortcut is created in your ShortcutPath location. If your TargetPath
+begins with `http` or a double backslash `\\` then a `.url` type shortcut is created.
+
+.PARAMETER TargetPath
+
+This is where the shortcut is going to.
+**MUST BEGIN WITH a driveletter (ex: F:\) or `http` or two backslashes `\\`!**
+
+.PARAMETER ShortcutPath
+
+This is where shortcuts will be located.
+
+.PARAMETER Name
+
+This is the name of your shortcut.
+
+.PARAMETER Description
+
+This is the description shown for the created shortcut.
+
+.PARAMETER IconPath
+
+This is the location of your `.ico` or `.dll` where your shortcut icon is located.
+If your pointing to a `.dll` then you'll need to know the IconIndex number to get the correct
+icon file.
+
+.PARAMETER IconIndex
+
+You'll need the IconIndex parameter in conjunction with IconPath. Default is 0.
+
+.EXAMPLE
+
+This is an example to make a shortcut on a users desktop to Sysinternals Process Explorer
+with a microchip icon from `SHELL32.dll`.
+
+$ShortcutSplat = @{
+    TargetPath   = '\\live.sysinternals.com\tools\procexp64.exe'
+    ShortcutPath = "$env:USERPROFILE\Desktop\"
+    Name         = 'Process Explorer'
+    IconPath     = "$env:SystemRoot\System32\SHELL32.dll"
+    IconIndex    = 12
+}
+New-ShortCut @ShortcutSplat
+
+.EXAMPLE
+
+The example is making a shortcut to the `C:\Windows\` folder in the documents folder.
+
+New-Shortcut -Targetpath 'C:\Windows' -ShortcutPath "$env:USERPROFILE\Documents\" -Name "Windows Folder"
+
+#>
     [CmdletBinding( DefaultParameterSetName = "Shortcut" )]
     param (
         [Parameter( Mandatory        = $true,
@@ -31,7 +93,7 @@ function New-ShortCut {
 
         [Parameter( Mandatory        = $true,
                     ParameterSetName = "Icon" )]
-        [int]$IconIndex
+        [int]$IconIndex = 0
     )
 
     try
@@ -41,11 +103,15 @@ function New-ShortCut {
             '.lnk'
         } elseif ( $TargetPath -match '^http' -or $TargetPath -match '\\\\' ) {
             '.url'
+        } else {
+            throw "Your shortcut MUST begin with a driveletter (ex: C:\), http or \\ !"
         }
 
         # Ensure shortcut path ends with backslash
         if ( $ShortcutPath -notmatch '\\$' ) {
+
             $ShortcutPath += '\'
+
         }
 
         $Full = "$ShortcutPath$Name$ext"
@@ -63,23 +129,28 @@ function New-ShortCut {
             $Shortcut.TargetPath = $TargetPath
 
             if ( $Description ) {
+
                 $Shortcut.Description = $Description
+
             }
-            
             if ( $IconPath ) {
+
                 $Shortcut.IconLocation = "$IconPath,$IconIndex"
+
             }
 
             $Shortcut.Save()
-            
+
         } else {
 
             # Create .url shortcut
 
     $URLFileContent = @"
         [InternetShortcut]
+        IDList=
         URL=$TargetPath
         IconIndex=$IconIndex
+        HotKey=0
         IconFile=$IconPath
 "@
 
@@ -94,8 +165,11 @@ function New-ShortCut {
         }
 
         Write-Verbose "Shortcut created successfully at: $Full"
+
     } catch {
+
         Write-Error "Error: $($_.Exception.Message)"
+
     } # End Try / Catch
 } # End Function
 
